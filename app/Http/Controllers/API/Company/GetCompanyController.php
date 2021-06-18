0a1dd6c591e889043257e7a07acaaa418ee260cf
ipulbelcram <ipulbelcram@gmail.com>
2021-06-18 16:22:28 +0700

    add get company all

--- app/Http/Controllers/API/Company/GetCompanyController.php
+++ app/Http/Controllers/API/Company/GetCompanyController.php
@@ -15,25 +15,31 @@ class GetCompanyController extends Controller
     public function __invoke(Request $request)
     {
         $this->validate($request, [
-            'type' => ['required', 'in:center,branch']
+            'type' => ['nullable', 'in:center,branch']
         ]);
 
         $user = User::find($request->user()->id);
         $company_code = ($user->role_id == 1) ? $user->company_code : $user->company_code_parent;
         $message = 'succes get company data';
         $company = Company::where('ref_company_code', $company_code);
-        if($request->type == 'center') {
-            $company = $company->where('type', 'center')->first();
-            return ResponseFormatter::success(
-                new CompanyResource($company),
-                $message
-            );
-        } else {
-            $company->where('type', 'branch');
-            return ResponseFormatter::success(
-                $company->get(),
-                $message
-            );
+        if($request->type) {
+            if($request->type == 'center') {
+                $company = $company->where('type', 'center')->first();
+                return ResponseFormatter::success(
+                    new CompanyResource($company),
+                    $message
+                );
+            } else {
+                $company->where('type', 'branch');
+                return ResponseFormatter::success(
+                    $company->get(),
+                    $message
+                );
+            }
         }
+        return ResponseFormatter::success(
+            $company->get(),
+            $message
+        );
     }
 }
