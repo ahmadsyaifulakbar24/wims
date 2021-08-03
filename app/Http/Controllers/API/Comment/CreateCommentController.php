daec5e7ab7531ed60bdca9ea7bfbfbdaf3220057
ahmadsyaifulakbar24 <ipulbelcram@gmail.com>
2021-08-03 12:39:07 +0700

    add user in comment

--- app/Http/Controllers/API/Comment/CreateCommentController.php
+++ app/Http/Controllers/API/Comment/CreateCommentController.php
@@ -7,16 +7,20 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\Task\CommentResource;
 use App\Models\Task;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
 
 class CreateCommentController extends Controller
 {
     public function __invoke(Request $request, Task $task)
     {
         $this->validate($request, [
-            'comment' => ['required', 'string']
+            'comment' => ['required', 'string'],
         ]);
 
-        $comment = $task->comment()->create($request->all());
+        $input = $request->all();
+        $input['user_id'] = Auth::user()->id;
+        $comment = $task->comment()->create($input);
         return ResponseFormatter::success(
             new CommentResource($comment),
             'success create comment'
