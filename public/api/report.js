77076647c6136c6bff4e06cc7e8d2733bdc26baa
Hilmi <nurhilmi.mail@gmail.com>
2021-08-20 10:24:30 +0700

    crud user report

---
+++ public/api/report.js
@@ -0,0 +1,328 @@
+get_data()
+
+function get_data() {
+    $('#table').empty()
+    $.ajax({
+        url: `${api_url}/user_report/fetch`,
+        type: 'GET',
+        data: {
+            user_id: user_id
+        },
+        success: function(result) {
+            // console.log(result.data)
+            if (result.data != '') {
+                $.each(result.data, function(index, value) {
+                    append = `<tr data-id="${value.id}" data-title="${value.title}">
+						<td class="text-center">${index + 1}.</td>
+						<td class="text-truncate">
+							<div class="text-primary detail" role="button">${value.title}</div>
+						</td>
+						<td class="d-flex align-items-center">
+							<i class="mdi mdi-24px mdi-pencil-outline pr-0 mr-2 edit" role="button"></i>
+							<i class="mdi mdi-24px mdi-trash-can-outline pr-0 delete" role="button"></i>
+						</td>
+					</tr>`
+                    $('#table').append(append)
+                })
+            } else {
+                append = `<td class="text-truncate" colspan="10">Data not found.</td>`
+                $('#table').append(append)
+            }
+        }
+    })
+}
+
+$('#add').click(function() {
+    $('#modal h5').html('Add Report')
+    $('form').attr('data-type', 'create')
+    $('#submit').html('Create')
+    $('#modal').modal('show')
+})
+
+$('.modal').on('shown.bs.modal', function() {
+    $('input:first').focus()
+})
+
+$('.modal').on('hidden.bs.modal', function() {
+    $('.is-invalid').removeClass('is-invalid')
+    $('.card-attachment').remove()
+    $('#title').val('')
+    attachments = []
+    attachment_staging()
+})
+
+let attachments = []
+$(document).on('change', 'input[type="file"]', function(e) {
+    attachments.push(this.files[0])
+    $('input[type="file"]').val('')
+    attachment_staging()
+})
+
+$(document).on('click', '.delete-staging-attachment', function(e) {
+    $(this).parents('.card').remove()
+    let id = $(this).parents('.card').attr('data-id')
+    if (id > -1) attachments.splice(id, 1)
+    $.each($('.card-staging-attachment'), function(index, value) {
+        $(this).attr('data-id', index)
+    })
+})
+
+function attachment_staging(value) {
+    $('.card-staging-attachment').remove()
+    $.each(attachments, function(index, value) {
+        let append = `<div class="card card-staging-attachment mb-2" data-id="${index}" data-title="${value.name}">
+			<div class="d-flex align-items-center">
+				<div class="d-flex align-items-center text-truncate my-3 ml-3" style="width: 90%" target="_blank">
+					<i class="mdi mdi-24px mdi-file-outline text-dark"></i>
+					<div class="text-truncate">${value.name}</div>
+				</div>
+				<i class="mdi mdi-24px mdi-trash-can-outline ml-auto delete-staging-attachment px-3" role="button"></i>
+			</div>
+		</div>`
+        $('#attachments').append(append)
+    })
+}
+
+$(document).on('click', '.edit', function() {
+    let id = $(this).parents('tr').attr('data-id')
+    let title = $(this).parents('tr').attr('data-title')
+    attachment_edit(id)
+    $('#title').val(title)
+    $('#modal h5').html('Edit Report')
+    $('#form').attr('data-id', id)
+    $('#form').attr('data-type', 'edit')
+    $('#submit').html('Save Changes')
+    $('#modal').modal('show')
+})
+
+function attachment_edit(id) {
+    $('#attachments').empty()
+    $.ajax({
+        url: `${api_url}/user_report/attachment/${id}/fetch`,
+        type: 'GET',
+        success: function(result) {
+            // console.log(result.data)
+            $.each(result.data, function(index, value) {
+                append = `<div class="card card-attachment mb-2" data-id="${value.id}" data-title="${value.name}">
+					<div class="d-flex align-items-center">
+						<a href="${value.file_url}" class="d-flex align-items-center text-truncate my-3 ml-3" style="width: 90%" target="_blank">
+							<i class="mdi mdi-24px mdi-file-outline text-dark"></i>
+							<div class="text-truncate">${value.name}</div>
+						</a>
+						<!--<i class="mdi mdi-24px mdi-trash-can-outline ml-auto delete-attachment px-3" role="button"></i>-->
+					</div>
+				</div>`
+                $('#attachments').append(append)
+            })
+        }
+    })
+}
+
+$(document).on('click', '.detail', function() {
+    let id = $(this).parents('tr').attr('data-id')
+    let title = $(this).parents('tr').attr('data-title')
+    attachment_detail(id)
+    get_comment(id)
+    $('#form-comment').attr('data-id', id)
+    $('#title-detail').html(title)
+    $('#modal-detail').modal('show')
+})
+
+function attachment_detail(id) {
+    $('#attachments-detail').empty()
+    $.ajax({
+        url: `${api_url}/user_report/attachment/${id}/fetch`,
+        type: 'GET',
+        success: function(result) {
+            // console.log(result.data)
+            if (result.data.length > 0) {
+                $.each(result.data, function(index, value) {
+                    append = `<div class="card card-attachment mb-2" data-id="${value.id}" data-title="${value.name}">
+						<div class="d-flex align-items-center">
+							<a href="${value.file_url}" class="d-flex align-items-center text-truncate my-3 ml-3" target="_blank">
+								<i class="mdi mdi-24px mdi-file-outline text-dark"></i>
+								<div class="text-truncate">${value.name}</div>
+							</a>
+						</div>
+					</div>`
+                    $('#attachments-detail').append(append)
+                })
+            } else {
+                let append = `<label class="card p-1">
+					<div class="d-flex align-items-center justify-content-center py-1 mb-0">
+						<span>No attachment</span>
+					</div>
+				</label>`
+                $('#attachments-detail').append(append)
+            }
+        }
+    })
+}
+
+$('#form').submit(function(e) {
+    e.preventDefault()
+    let id = $(this).attr('data-id')
+    let type = $(this).attr('data-type')
+    $('#submit').attr('disabled', true)
+    if (type == 'create') {
+        let formData = new FormData()
+        formData.append('user_id', user_id)
+        formData.append('title', $('#title').val())
+        $.ajax({
+            url: `${api_url}/user_report/create`,
+            type: 'POST',
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function(result) {
+                if (attachments.length > 0) {
+                    $.each(attachments, function(index, value) {
+                        let formData = new FormData()
+                        formData.append('attachment', value)
+                        $.ajax({
+                            url: `${api_url}/user_report/attachment/${result.data.id}/create`,
+                            type: 'POST',
+                            data: formData,
+                            processData: false,
+                            contentType: false,
+                        })
+                    })
+                }
+                $('#modal').modal('hide')
+                customAlert('success', 'Report added')
+                get_data()
+            },
+            error: function(xhr) {
+                // console.log(xhr)
+                let err = xhr.responseJSON.errors
+                if (err.title) {
+                    $('#title').addClass('is-invalid')
+                    $('#title').siblings('.invalid-feedback').html(err.title)
+                }
+            },
+            complete: function() {
+                $('#submit').attr('disabled', false)
+            }
+        })
+    } else {
+        $.ajax({
+            url: `${api_url}/user_report/${id}/update`,
+            type: 'PATCH',
+            data: {
+                title: $('#title').val(),
+            },
+            success: function(result) {
+                if (attachments.length > 0) {
+                    $.each(attachments, function(index, value) {
+                        let formData = new FormData()
+                        formData.append('attachment', value)
+                        $.ajax({
+                            url: `${api_url}/user_report/attachment/${result.data.id}/create`,
+                            type: 'POST',
+                            data: formData,
+                            processData: false,
+                            contentType: false,
+                        })
+                    })
+                }
+                $('#modal').modal('hide')
+                customAlert('success', 'Report updated')
+                get_data()
+            },
+            error: function(xhr) {
+                // console.log(xhr)
+                let err = xhr.responseJSON.errors
+                if (err.title) {
+                    $('#title').addClass('is-invalid')
+                    $('#title').siblings('.invalid-feedback').html(err.title)
+                }
+            },
+            complete: function() {
+                $('#submit').attr('disabled', false)
+            }
+        })
+    }
+})
+
+$(document).on('click', '.delete', function() {
+    let id = $(this).parents('tr').attr('data-id')
+    let title = $(this).parents('tr').attr('data-title')
+    $('#delete').attr('data-id', id)
+    $('#modal-delete .modal-body b').html(title)
+    $('#modal-delete').modal('show')
+})
+
+$(document).on('click', '#delete', function() {
+    let id = $('#delete').attr('data-id')
+    $('#delete').attr('disabled', true)
+    $.ajax({
+        url: `${api_url}/user_report/${id}/archive`,
+        type: 'DELETE',
+        success: function(result) {
+            $('#modal-delete').modal('hide')
+            $('#delete').attr('disabled', false)
+            customAlert('success', 'Report deleted')
+            get_data()
+        }
+    })
+})
+
+function get_comment(report_id) {
+    $('#comment-detail').empty()
+    $.ajax({
+        url: `${api_url}/user_report/comment/${report_id}/fetch`,
+        type: 'GET',
+        success: function(result) {
+            // console.log(result)
+            $.each(result.data, function(index, value) {
+                append = `<div class="d-flex align-items-start mb-3" data-id="${value.id}" data-title="${value.comment}">
+					<img src="${value.user.profile_photo_url}" class="rounded-circle mb-1" width="30" alt="">
+					<div class="ml-3">
+						<div><b>${value.user.name}</b> <small class="text-secondary">${date_format(value.created_at.substr(0, 10))}</small></div>
+						<div>${value.comment}</div>
+					</div>
+					<!--<div class="dropdown ml-auto">
+						<i class="mdi mdi-24px mdi-dots-horizontal pr-0" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" role="button"></i>
+						<div class="dropdown-menu dropdown-menu-right py-0">
+							<div class="dropdown-item edit-comment" role="button">Edit</div>
+							<div class="dropdown-item delete-comment" role="button">Delete</div>
+						</div>
+					</div>-->
+				</div>`
+                $('#comment-detail').prepend(append)
+            })
+        }
+    })
+}
+
+$('#form-comment').submit(function(e) {
+    e.preventDefault()
+    $('#submit-comment').attr('disabled', true)
+    $('.is-invalid').removeClass('is-invalid')
+    let report_id = $('#form-comment').attr('data-id')
+    let formData = new FormData()
+    formData.append('comment', $('#comment').val())
+    $.ajax({
+        url: `${api_url}/user_report/comment/${report_id}/create`,
+        type: 'POST',
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function(result) {
+            $('#comment').val('')
+            customAlert('success', `Comment submited`)
+            get_comment(report_id)
+        },
+        error: function(xhr) {
+            let err = xhr.responseJSON.errors
+            // console.log(err)
+            if (err.comment) {
+                $('#comment').addClass('is-invalid')
+                $('#comment').siblings('.invalid-feedback').html(err.comment)
+            }
+        },
+        complete: function() {
+            $('#submit-comment').attr('disabled', false)
+        }
+    })
+})
---
+++ resources/views/report.blade.php
@@ -0,0 +1,140 @@
+@extends('layouts/app')
+
+@section('title','Report')
+
+@section('content')
+	<div class="container">
+		<div class="card">
+			<div class="card-header border-bottom-0">
+				<div class="d-flex align-items-center justify-content-between">
+					<h4 class="mb-0">Report</h4>
+					<div class="d-flex align-items-center">
+						<!-- <i class="mdi mdi-24px mdi-magnify" role="button"></i> -->
+						<i class="mdi mdi-24px mdi-plus-circle-outline pr-0" id="add" data-toggle="tooltip" data-placement="bottom" title="Add Report" role="button"></i>
+					</div>
+				</div>
+			</div>
+			<div class="table-responsive">
+				<table class="table table-middle">
+					<thead>
+						<tr>
+							<th class="text-truncate text-center">No.</th>
+							<th class="text-truncate">Title</th>
+							<th class="text-truncate"></th>
+						</tr>
+					</thead>
+					<tbody id="table"></tbody>
+				</table>
+			</div>
+			<!-- <div class="d-flex flex-column justify-content-center align-items-center py-5">
+				<div class="loader loader-sm">
+					<svg class="circular" viewBox="25 25 50 50">
+						<circle class="path-dark" cx="50" cy="50" r="20" fill="none" stroke-width="5" stroke-miterlimit="10"/>
+					</svg>
+				</div>
+			</div> -->
+		</div>
+	</div>
+	<div class="modal fade" id="modal" tabindex="-1" aria-hidden="true">
+	    <div class="modal-dialog modal-dialog-centered">
+	        <div class="modal-content">
+	            <div class="modal-header border-bottom-0">
+	                <h5 class="modal-title"></h5>
+	                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	                    <i class="mdi mdi-close pr-0"></i>
+	                </button>
+	            </div>
+	            <form id="form">
+		            <div class="modal-body">
+						<div class="form-group">
+							<label for="title">Title</label>
+							<input class="form-control" id="title">
+							<div class="invalid-feedback"></div>
+						</div>
+						<div class="form-group">
+							<label for="title">Attachment</label>
+							<div id="attachments"></div>
+							<label class="card p-1" for="attachment" role="button">
+								<div class="d-flex align-items-center justify-content-center mb-0" role="button">
+									<i class="mdi mdi-18px mdi-plus"></i>
+									<span>Add attachment</span>
+								</div>
+							</label>
+							<input type="file" class="none" id="attachment" accept="image/*, application/msword, application/vnd.ms-excel, application/vnd.ms-powerpoint, application/vnd.openxmlformats-officedocument.wordprocessingml.document, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, .pptx, application/pdf" role="button">
+						</div>
+		            </div>
+		            <div class="modal-footer border-top-0">
+		                <button class="btn btn-outline-dark" data-dismiss="modal">Close</button>
+		                <button class="btn btn-dark" id="submit">Create</button>
+		            </div>
+		        </form>
+	        </div>
+	    </div>
+	</div>
+	<div class="modal fade" id="modal-delete" tabindex="-1" aria-hidden="true">
+	    <div class="modal-sm modal-dialog modal-dialog-centered">
+	        <div class="modal-content">
+	            <div class="modal-header border-bottom-0">
+	                <h5 class="modal-title">Delete Report</h5>
+	                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	                    <i class="mdi mdi-close pr-0"></i>
+	                </button>
+	            </div>
+	            <div class="modal-body">
+	            	Are you sure want to delete <b></b>?
+	            </div>
+	            <div class="modal-footer border-top-0">
+	                <button class="btn btn-outline-dark" data-dismiss="modal">Close</button>
+	                <button class="btn btn-dark" id="delete">Delete</button>
+	            </div>
+	        </div>
+	    </div>
+	</div>
+	<div class="modal fade" id="modal-detail" tabindex="-1" aria-hidden="true">
+	    <div class="modal-dialog modal-dialog-centered">
+	        <div class="modal-content">
+	            <div class="modal-header border-bottom-0">
+	                <h5 class="modal-title">Detail Report</h5>
+	                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	                    <i class="mdi mdi-close pr-0"></i>
+	                </button>
+	            </div>
+	            <div class="modal-body">
+					<div class="form-group">
+						<label>Title</label>
+						<div class="font-weight-bold" id="title-detail"></div>
+					</div>
+					<div class="form-group">
+						<label>Attachment</label>
+						<div id="attachments-detail"></div>
+					</div>
+					<div class="form-group">
+						<label for="comment">Comment</label>
+						<form id="form-comment">
+							<div class="d-flex align-items-start mb-3">
+								<img class="avatar rounded-circle mt-1" width="30" alt="">
+								<div class="input-group ml-3">
+									<input class="form-control" id="comment" placeholder="Write a comment...">
+									<div class="input-group-append" id="submit-comment">
+										<button class="btn btn-sm btn-dark rounded-right">Reply</button>
+									</div>
+									<div class="invalid-feedback"></div>
+								</div>
+							</div>
+						</form>
+						<div id="comment-detail"></div>
+	            </div>
+	        </div>
+	    </div>
+	</div>
+	<script src="https://unpkg.com/@popperjs/core@2"></script>
+@endsection
+
+@section('script')
+	<script src="{{asset('api/report.js')}}"></script>
+	<script>
+		$(function () {
+			$('[data-toggle="tooltip"]').tooltip()
+		})
+	</script>
+@endsection
